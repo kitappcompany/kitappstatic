a1d1ec6cd423289007860370d22ae72d55beeef1
zakiraghayev <agayevzakir239@gmail.com>
2020-01-09 04:07:12 +0000

    msg

--- chat.js
+++ chat.js
@@ -1,14 +1,15 @@
-function message() {
+function messageStart(reciverEmail, book_id) {
     wsStart = 'ws://'
     if (true) {
         wsStart = 'wss://'
     }
     endpoint = wsStart + window.location.host + "/chatSocket";
     let socket = new WebSocket(endpoint);
-
+    let text   = document.querySelector("#message-input").value;
     socket.onopen = function (e) {
+        socket.send(JSON.stringify({"reciver":reciverEmail, "book":book_id, "text":text}))
         // body...
-        console.log("open", e)
+        socket.close()
     }
     socket.onmessage = function (e) {
         // body...
