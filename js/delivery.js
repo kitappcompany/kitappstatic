6a06d8804b8f8d05f84f4304560c7c4df4a54be5
kitappcompany <agayevzakir239@gmail.com>
2020-02-07 10:10:34 +0000

    locations

--- js/delivery.js
+++ js/delivery.js
@@ -1,19 +1,32 @@
 let deliveryContainer = document.querySelector('.delivery-container');
 let locationInputTemp = Handlebars.compile(`<div class="delivery d-flex align-items-center py-2">
-            <input value="{{loc_name}}" list="suggestionsholder" type="text" class = "input-val" oninput="SuggestPlace(event)">
+            <input value="{{loc_name}}" list="suggestionsholder" type="text" class = "input-val locations">
             <img src="https://cdn.jsdelivr.net/gh/kitappcompany/kitappstatic@latest/icons/Path 413.svg" alt="" class = "ml-3 plas-delivery" onclick = "addDelivery();return false;">
-
-            <ul class = "drop-down-list list-unstyled text-left text-capitalize">
-
-            </ul>
-
             </div>`);
 
 
 function addDelivery(loc_name=null){
     deliveryContainer.insertAdjacentHTML('beforeend', locationInputTemp({'loc_name':loc_name}) ) ;
+    auto_complete();
 }
 
+
+function auto_complete() {
+    // body...
+    let urls = "https://places.sit.ls.hereapi.com/places/v1/autosuggest?in=40%2C49%3Br%3D500000&size=5&result_types=address%2C+place%2C+chain%2C+category&tf=plain&";
+    let parameters = "q="+ input.value + "&apikey=y9kQaWgzK5EwZQTAYxYio7sLA1lPIGW013LxMQg_qCM"
+    var options = {
+          url: urls+parameters,
+          getValue: "title",
+          list: {
+            match: {
+              enabled: true
+            }
+          },
+          theme: "square"
+        };
+    $(".locations").easyAutocomplete(options);
+}
 
 // /MAPS PLACE API
 function SuggestPlace(event) {
@@ -21,25 +34,42 @@ function SuggestPlace(event) {
         return
     }
     else{
-            var input = event.target;
-            let url = "https://places.sit.ls.hereapi.com/places/v1/autosuggest?in=40%2C49%3Br%3D500000&size=5&result_types=address%2C+place%2C+chain%2C+category&tf=plain&";
-            let parameters = "q="+ input.value + "&apikey=y9kQaWgzK5EwZQTAYxYio7sLA1lPIGW013LxMQg_qCM"
-            const request = new XMLHttpRequest();
-            request.open("GET", url+parameters)
-
-            request.onload = ()=>{
-                // init some parameters
-                let res = JSON.parse(request.responseText),
-                dataAll = res["results"],
-                optionsPlace = document.querySelector("#suggestionsholder");
-
-                const temp = Handlebars.compile('{{#each names}} {{#if this.id}} <option data-position="{{this.position}}" data-pk="{{this.id}}" value="{{this.title}}"> {{this.vicinity}} </option>{{/if}} {{/each}}')
-                // fill datalist with options suggested by API HERE
-                optionsPlace.innerHTML = temp({"names":dataAll});
-
+        let urls = "https://places.sit.ls.hereapi.com/places/v1/autosuggest?in=40%2C49%3Br%3D500000&size=5&result_types=address%2C+place%2C+chain%2C+category&tf=plain&";
+        let parameters = "q="+ input.value + "&apikey=y9kQaWgzK5EwZQTAYxYio7sLA1lPIGW013LxMQg_qCM"
+        var options = {
+          url: urls+parameters,
+          getValue: "title",
+          list: {
+            match: {
+              enabled: true
             }
-            request.send()
-        }
+          },
+          theme: "square"
+        };
+        $(".locations").easyAutocomplete(options);
+
+    }
+
+    // else{
+    //         var input = event.target;
+    //         let url = "https://places.sit.ls.hereapi.com/places/v1/autosuggest?in=40%2C49%3Br%3D500000&size=5&result_types=address%2C+place%2C+chain%2C+category&tf=plain&";
+    //         let parameters = "q="+ input.value + "&apikey=y9kQaWgzK5EwZQTAYxYio7sLA1lPIGW013LxMQg_qCM"
+    //         const request = new XMLHttpRequest();
+    //         request.open("GET", url+parameters)
+
+    //         request.onload = ()=>{
+    //             // init some parameters
+    //             let res = JSON.parse(request.responseText),
+    //             dataAll = res["results"],
+    //             optionsPlace = document.querySelector("#suggestionsholder");
+
+    //             const temp = Handlebars.compile('{{#each names}} {{#if this.id}} <option data-position="{{this.position}}" data-pk="{{this.id}}" value="{{this.title}}"> {{this.vicinity}} </option>{{/if}} {{/each}}')
+    //             // fill datalist with options suggested by API HERE
+    //             optionsPlace.innerHTML = temp({"names":dataAll});
+
+    //         }
+    //         request.send()
+    //     }
 }
 
 
