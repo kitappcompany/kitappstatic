f0bbcd89d9237ee893cb54cd144b01411c5c071a
kitappcompany <agayevzakir239@gmail.com>
2020-02-21 01:06:06 +0000

    new css fonts icons

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,34 +1,27 @@
-function tooltip() {
-    let inputFocus = document.querySelectorAll('.input-focus'),
-    inputBorder = document.querySelectorAll(".input-border");
-    let tooltip = document.querySelector('.tooltip-remoteness');
+let inputFocus = document.querySelectorAll('.input-focus'),
+inputBorder = document.querySelectorAll(".input-border");
+let tooltip = document.querySelector('.tooltip-remoteness');
 
-    for(let i = 0;i < inputFocus.length;i++){
-        inputFocus[i].onfocus = function(){
-            inputBorder[i].style.border = ".1rem solid #EFEFEF";
-            inputBorder[i].style.background = "#EFEFEF";
-            if(inputFocus[i].classList.contains('tooltip-focus')){
-                tooltip.style.opacity = "1";
-                tooltip.style.zIndex = "15";
-                setTimeout(function(){
-                    tooltip.style.opacity = "0";
-                    tooltip.style.zIndex = "10";
-                },2000)
-            }
-
-        }
-        inputFocus[i].onblur = function(){
-            inputBorder[i].style.background = "#fff";
-            inputBorder[i].style.border = ".1rem solid #b5b5b5";
-        }
+function inputOnFocus(inputF){
+    inputF.parentElement.style.border = ".1rem solid #EFEFEF";
+    inputF.parentElement.style.background = "#EFEFEF";
+    if(inputF.classList.contains('tooltip-focus')){
+        tooltip.style.opacity = "1";
+        tooltip.style.zIndex = "15";
     }
-
-    // qeleme click olunduqda inputa focus
-    // let pencil = document.querySelector('.pencil'),pencilInput = document.querySelector('.pencil-input');
-    // pencil.onclick = function(){
-    //     pencilInput.focus();
-    // }
-
 }
+function inputOnBlur(inputF){
+    inputF.parentElement.style.border = ".1rem solid #b5b5b5";
+    inputF.parentElement.style.background = "#fff";
+    tooltip.style.opacity = "0";
+    tooltip.style.zIndex = "10";
+}
+
+setInterval(function(){
+    if(inputFocus[2].value.length != 0){
+        tooltip.style.opacity = "0";
+        tooltip.style.zIndex = "10";
+    }
+},100)
 
 
