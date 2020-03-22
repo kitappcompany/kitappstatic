0e1eae86313da15fb37a0f3e97aeb9ced1bfed10
kitappcompany <agayevzakir239@gmail.com>
2020-03-22 13:46:21 +0000

    bookmarks.js

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -1,48 +1,38 @@
 
-// click olunduqda bookmark olmasi
+var next_page_books = '/bookmark-api/listbooks';
 
-let bookMark = document.querySelectorAll(".bookmark"),bookCard = document.querySelectorAll(".book-card");
-let bookMarkIndex = [];
-for(let i = 0;i < bookMark.length;i++) bookMarkIndex[i] = false;
-
-for(let i = 0;i < bookMark.length;i++){
-    if(!bookMarkIndex[i]){
-        bookMark[i].setAttribute("src", "https://cdn.jsdelivr.net/gh/kitappcompany/kitappstatic@latest/img/bookmark-red.svg");
-        bookMarkIndex[i] = true;
-        bookMark[i].style.opacity = "1";
-    }
-    else{
-        bookMark[i].setAttribute("src", "https://cdn.jsdelivr.net/gh/kitappcompany/kitappstatic@latest/img/bookmark.svg");
-        bookMarkIndex[i] = false;
+function ListBooks() {
+    // body...
+    if (!next_page_books) {
+        loadingBar[0].style.display = "none";
+        return
     }
 
-}
+    const request = new XMLHttpRequest();
+    request.open("GET", next_page_books, true);
 
-for(let i = 0;i < bookMark.length;i++){
-    bookMark[i].onclick = function(){
-        if(!bookMarkIndex[i]){
-            bookMark[i].setAttribute("src", "/static/icons/bookmark-red.svg");
-            bookMarkIndex[i] = true;
+
+    request.onload = ()=>{
+
+        try {
+            /* code */
+
+            let res = JSON.parse(request.responseText)
+            next_page_books = res['next']
+
+            const temp = Handlebars.compile(document.querySelector("#bookmark-instance").innerHTML);
+            document.querySelector("#book-sell").children[0].innerHTML +=temp({"book":res.results})
+            bookmark(); // add bookmark functionality to book cards (bookmark.js)
+
+            loadingBar[0].style.display = "none";
+            loadMore[0].style.display = "inline-block";
+
+        } catch (e) {
+            console.log(e)
+            loadingBar[0].style.display = "none";
+            loadMore[0].style.display = "inline-block";
         }
-        else{
-            bookMark[i].setAttribute("src", "/static/icons/bookmark.svg");
-            bookMarkIndex[i] = false;
-        }
-
-    }
-    if(bookMarkIndex[i]){
-        bookMark[i].style.top = "100%";
-    }
-    bookCard[i].onmouseover = function(){
-        bookMark[i].style.top = "0";
-        bookMark[i].style.opacity = "1";
     }
 
-        bookCard[i].onmouseout = function(){
-            bookMark[i].style.opacity = "0";
-            if(bookMarkIndex[i]){
-            bookMark[i].style.top = "100%";
-            bookMark[i].style.opacity = "1";
-            }
-        }
+    request.send()
 }
