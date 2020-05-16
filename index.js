a913bc2a5e5cf7949b9ea11276dc5fde2343e994
kitappcompany <agayevzakir239@gmail.com>
2020-05-16 14:20:36 +0000

    filterset

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     let lis = document.querySelectorAll(".genre-proven");
 
     for (var i = 0; i < lis.length; i++) {
-        lis[i].addEventListener('click',BooksList("/catalog-api/listbooks" + "?genre__id="+ lis.dataset.genreid), false);
+        lis[i].addEventListener('click',BooksList("/catalog-api/listbooks" + "?genre__id="+ lis[i].dataset.genreid), false);
     }
 })
 
