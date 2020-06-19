6a91b01d80e84916304f7250bf2d22b9bad7170d
kitappcompany <agayevzakir239@gmail.com>
2020-06-19 11:58:56 +0000

    popupmap

--- js/popup-delivery.js
+++ js/popup-delivery.js
@@ -8,7 +8,11 @@ function popupdelivery() {
 
     deliveryButton.onclick = function(){
         if(!clickCheck){
-            openDelivery();
+           popupMap.style.display = "block";
+            deliveryButton.style.borderBottom = ".1rem solid #707070";
+            deliveryButton.style.color = "#686868";
+            popupContainer.style.top = "72%";
+            clickCheck = true;
 
             // Show map and add locations
             //if once run then no need to run again for each post
@@ -19,24 +23,11 @@ function popupdelivery() {
 
         }
         else{
-            closeDelivery();
+             popupMap.style.display = "none";
+                popupContainer.style.top = "50%";
+                deliveryButton.style.borderBottom = "none";
+                deliveryButton.style.color = "#5CB4F1";
+                clickCheck = false;
         }
     }
 }//function end
-
-//xeritenin acilmasi
-function openDelivery(){
-    popupMap.style.display = "block";
-    deliveryButton.style.borderBottom = ".1rem solid #707070";
-    deliveryButton.style.color = "#686868";
-    popupContainer.style.top = "72%";
-    clickCheck = true;
-}
-//xeritenin baglanmasi
-function closeDelivery(){
-    popupMap.style.display = "none";
-    popupContainer.style.top = "50%";
-    deliveryButton.style.borderBottom = "none";
-    deliveryButton.style.color = "#5CB4F1";
-    clickCheck = false;
-}
