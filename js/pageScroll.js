c9e5d704a622a2f90e881143751af6e5fa9d1147
Zakir Aghayev <agayevzakir239@gmail.com>
2020-09-09 17:35:21 +0000

    donedone

---
+++ js/pageScroll.js
@@ -0,0 +1,8 @@
+function goToBack(){
+    window.scrollTo(0, 0);
+}
+
+window.addEventListener("scroll",function(){
+    if(window.scrollY != 0) document.querySelector(".go-to-back").style.display = "flex";
+    else document.querySelector(".go-to-back").style.display = "none";
+});
